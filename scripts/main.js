93a7baf63e366ca0e524878e7efe19d5a58fea25
Christian Kitte <chkitte@web.de>
2022-05-04 20:31:30 +0200

    Erste Arbeiten am Netzwerk

--- scripts/main.js
+++ scripts/main.js
@@ -363,7 +363,6 @@ function RandomTest() {
         // Set input current in mA
         if (t > 10 && t < 180) {
             i_app = random();
-            console.log(i_app);
         } else {
             i_app = 0.0;
         }
@@ -383,9 +382,9 @@ function RandomTest() {
 
 // Ein LIF als synaptisches Neuron
 // Es empfängt ein Spike als Input und behält es für eine bestimmte Zeit
-class LIFSynapticNeuron extends LIFNeuron() {
+class LIFSynapticNeuron extends LIFNeuron {
     constructor(n_syn, w, max_spikes = 50, u_rest = 0.0, u_thresh = 1.0, tau_rest = 4.0, r = 1.0, tau = 10.0, q = 1.5, tau_syn = 10.0) {
-        super(u_rest = 0.0, u_thresh = 1.0, tau_rest = 4.0, r = 1.0, tau);
+        super(u_rest, u_thresh, tau_rest, r, tau);
 
         // Number of synapses
         this.n_syn = n_syn;
@@ -401,7 +400,6 @@ class LIFSynapticNeuron extends LIFNeuron() {
 
     // Update the parent graph variables and placeholders
     get_vars_and_ph() {
-
         // The history of synaptic spike times for the neuron
         // tf.Variable(tf.constant(-1.0, shape=[self.max_spikes, self.n_syn], dtype=tf.float32))
         //
@@ -409,6 +407,7 @@ class LIFSynapticNeuron extends LIFNeuron() {
         // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array
         //
         // Ein Array Anzahl der Synapsen (Vertikal) mal Anzahl gemerkter Spikes (horizontal)
+        // mit -1 vorbelegt
         //
         this.t_spikes = new Array(this.n_syn);
         for (let i = 0; i < this.max_spikes; i++) {
@@ -425,53 +424,49 @@ class LIFSynapticNeuron extends LIFNeuron() {
 
         // A placeholder indicating which synapse spiked in the last time step
         // tf.placeholder(shape=[self.n_syn], dtype=tf.bool)
-        this.syn_has_spiked;
+        //
+        // Ein Array Anzahl der Synapsen (Vertikal)
+        // mit -1 vorbelegt
+        //
+        this.syn_has_spiked = new Array(this.n_syn);
     }
 
     // Operation to update spike times
     update_spike_times() {
-        /*
+        // Increase the age of older spikes
+        // old_spikes_op = self.t_spikes.assign_add(tf.where(self.t_spikes >=0,
+        //                                                   tf.constant(1.0, shape=[self.max_spikes, self.n_syn]) * self.dt,
+        //                                                   tf.zeros([self.max_spikes, self.n_syn])))
 
-          # Increase the age of older spikes
-          old_spikes_op = self.t_spikes.assign_add(tf.where(self.t_spikes >=0,
-                                                            tf.constant(1.0, shape=[self.max_spikes, self.n_syn]) * self.dt,
-                                                            tf.zeros([self.max_spikes, self.n_syn])))
+        // Increment last spike index (modulo max_spikes)
+        // new_idx_op = self.t_spikes_idx.assign(tf.mod(self.t_spikes_idx + 1, self.max_spikes))
 
-          # Increment last spike index (modulo max_spikes)
-          new_idx_op = self.t_spikes_idx.assign(tf.mod(self.t_spikes_idx + 1, self.max_spikes))
+        // Create a list of coordinates to insert the new spikes
+        // idx_op = tf.constant(1, shape=[self.n_syn], dtype=tf.int32) * new_idx_op
+        // coord_op = tf.stack([idx_op, tf.range(self.n_syn)], axis=1)
 
-          # Create a list of coordinates to insert the new spikes
-          idx_op = tf.constant(1, shape=[self.n_syn], dtype=tf.int32) * new_idx_op
-          coord_op = tf.stack([idx_op, tf.range(self.n_syn)], axis=1)
+        // Create a vector of new spike times (non-spikes are assigned a negative time)
+        // new_spikes_op = tf.where(self.syn_has_spiked,
+        //                          tf.constant(0.0, shape=[self.n_syn]),
+        //                          tf.constant(-1.0, shape=[self.n_syn]))
 
-          # Create a vector of new spike times (non-spikes are assigned a negative time)
-          new_spikes_op = tf.where(self.syn_has_spiked,
-                                   tf.constant(0.0, shape=[self.n_syn]),
-                                   tf.constant(-1.0, shape=[self.n_syn]))
-
-          # Replace older spikes by new ones
-          return tf.scatter_nd_update(old_spikes_op, coord_op, new_spikes_op)
-
-        */
+        // Replace older spikes by new ones
+        // return tf.scatter_nd_update(old_spikes_op, coord_op, new_spikes_op)
     }
 
     // Override parent get_input_op method
     get_input_op() {
-        /*
+        // Update our memory of spike times with the new spikes
+        //t_spikes_op = self.update_spike_times()
 
-                # Update our memory of spike times with the new spikes
-        t_spikes_op = self.update_spike_times()
+        // Evaluate synaptic input current for each spike on each synapse
+        // i_syn_op = tf.where(t_spikes_op >=0,
+        //                    self.q/self.tau_syn * tf.exp(tf.negative(t_spikes_op/self.tau_syn)),
+        //                    t_spikes_op*0.0)
 
-        # Evaluate synaptic input current for each spike on each synapse
-        i_syn_op = tf.where(t_spikes_op >=0,
-                            self.q/self.tau_syn * tf.exp(tf.negative(t_spikes_op/self.tau_syn)),
-                            t_spikes_op*0.0)
+        // Add each synaptic current to the input current
+        //i_op =  tf.reduce_sum(self.w * i_syn_op)
 
-        # Add each synaptic current to the input current
-        i_op =  tf.reduce_sum(self.w * i_syn_op)
-
-        return tf.add(self.i_app, i_op)
-
-        */
+        //return tf.add(self.i_app, i_op)
     }
 }
