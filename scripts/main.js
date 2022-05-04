1676a72e0a31c04df931880c1255129ee62c2fb9
Christian Kitte <chkitte@web.de>
2022-05-04 16:11:52 +0200

    Start mit Konvertierung LIF Synaptic Neuron, Random befeuerung und Ausgabe LIF

--- scripts/main.js
+++ scripts/main.js
@@ -191,7 +191,6 @@ class LIFNeuron {
         // Membranwiderstand in Ohm
         this.r = r = 1.0;
 
-
         // The duration left in the resting period (0 most of the time except after a neuron spike)
         // Die in der Ruhephase verbleibende Dauer (0 für die meiste Zeit, außer nach einem Neuronenspike)
         this.t_rest = 0.0;
@@ -335,10 +334,10 @@ function pulseTest() {
         U.push(neuron.u);
     }
 
-    DrawGraph("Diagramm", I, U, timePoint);
+    DrawGraph("Diagramm", U, I, timePoint);
 }
 
-// Simulation with square input currents
+// Simulation with Random Current
 function RandomTest() {
     // Audiocontext, need a manual interaction within the Browser
     myAudioContext = new AudioContext();
@@ -354,6 +353,7 @@ function RandomTest() {
     let U = [];
 
     let neuron = new LIFNeuron();
+    const random = d3.randomNormal(1.5, 1.0);
 
     for (x = 1; x <= steps; x++) {
         let i_app = 0;
@@ -362,7 +362,8 @@ function RandomTest() {
 
         // Set input current in mA
         if (t > 10 && t < 180) {
-            i_app = d3.randomUniform(-1, 3);
+            i_app = random();
+            console.log(i_app);
         } else {
             i_app = 0.0;
         }
@@ -377,5 +378,100 @@ function RandomTest() {
         U.push(neuron.u);
     }
 
-    DrawGraph("Diagramm", I, U, timePoint);
+    DrawGraph("Diagramm", U, I, timePoint);
+}
+
+// Ein LIF als synaptisches Neuron
+// Es empfängt ein Spike als Input und behält es für eine bestimmte Zeit
+class LIFSynapticNeuron extends LIFNeuron() {
+    constructor(n_syn, w, max_spikes = 50, u_rest = 0.0, u_thresh = 1.0, tau_rest = 4.0, r = 1.0, tau = 10.0, q = 1.5, tau_syn = 10.0) {
+        super(u_rest = 0.0, u_thresh = 1.0, tau_rest = 4.0, r = 1.0, tau);
+
+        // Number of synapses
+        this.n_syn = n_syn;
+        // The synaptic efficacy
+        this.w = w;
+        // Maximum number of spikes we remember
+        this.max_spikes = max_spikes;
+        // The neuron synaptic 'charge'
+        this.q = q;
+        // The synaptic time constant (ms)
+        this.tau_syn = tau_syn;
+    }
+
+    // Update the parent graph variables and placeholders
+    get_vars_and_ph() {
+
+        // The history of synaptic spike times for the neuron
+        // tf.Variable(tf.constant(-1.0, shape=[self.max_spikes, self.n_syn], dtype=tf.float32))
+        //
+        // https://www.w3docs.com/snippets/javascript/how-to-create-a-two-dimensional-array-in-javascript.html
+        // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array
+        //
+        // Ein Array Anzahl der Synapsen (Vertikal) mal Anzahl gemerkter Spikes (horizontal)
+        //
+        this.t_spikes = new Array(this.n_syn);
+        for (let i = 0; i < this.max_spikes; i++) {
+            this.t_spikes[i] = new Array(this.max_spikes);
+
+            for (let ii = 0; ii < this.max_spikes; ii++) {
+                this.t_spikes[i][ii] = -1;
+            }
+        }
+
+        // The last index used to insert spike times
+        // tf.Variable(self.max_spikes-1, dtype=tf.int32)
+        this.t_spikes_idx = this.max_spikes - 1;
+
+        // A placeholder indicating which synapse spiked in the last time step
+        // tf.placeholder(shape=[self.n_syn], dtype=tf.bool)
+        this.syn_has_spiked;
+    }
+
+    // Operation to update spike times
+    update_spike_times() {
+        /*
+
+          # Increase the age of older spikes
+          old_spikes_op = self.t_spikes.assign_add(tf.where(self.t_spikes >=0,
+                                                            tf.constant(1.0, shape=[self.max_spikes, self.n_syn]) * self.dt,
+                                                            tf.zeros([self.max_spikes, self.n_syn])))
+
+          # Increment last spike index (modulo max_spikes)
+          new_idx_op = self.t_spikes_idx.assign(tf.mod(self.t_spikes_idx + 1, self.max_spikes))
+
+          # Create a list of coordinates to insert the new spikes
+          idx_op = tf.constant(1, shape=[self.n_syn], dtype=tf.int32) * new_idx_op
+          coord_op = tf.stack([idx_op, tf.range(self.n_syn)], axis=1)
+
+          # Create a vector of new spike times (non-spikes are assigned a negative time)
+          new_spikes_op = tf.where(self.syn_has_spiked,
+                                   tf.constant(0.0, shape=[self.n_syn]),
+                                   tf.constant(-1.0, shape=[self.n_syn]))
+
+          # Replace older spikes by new ones
+          return tf.scatter_nd_update(old_spikes_op, coord_op, new_spikes_op)
+
+        */
+    }
+
+    // Override parent get_input_op method
+    get_input_op() {
+        /*
+
+                # Update our memory of spike times with the new spikes
+        t_spikes_op = self.update_spike_times()
+
+        # Evaluate synaptic input current for each spike on each synapse
+        i_syn_op = tf.where(t_spikes_op >=0,
+                            self.q/self.tau_syn * tf.exp(tf.negative(t_spikes_op/self.tau_syn)),
+                            t_spikes_op*0.0)
+
+        # Add each synaptic current to the input current
+        i_op =  tf.reduce_sum(self.w * i_syn_op)
+
+        return tf.add(self.i_app, i_op)
+
+        */
+    }
 }
