ac5ddb5d0e78284ad715617c4483d0da9a5369ad
Christian Kitte <chkitte@web.de>
2022-05-05 15:45:19 +0200

    Start NEtzwerk aus LIF

--- scripts/main.js
+++ scripts/main.js
@@ -409,9 +409,9 @@ class LIFSynapticNeuron extends LIFNeuron {
         // Ein Array Anzahl der Synapsen (Vertikal) mal Anzahl gemerkter Spikes (horizontal)
         // mit -1 vorbelegt
         //
-        this.t_spikes = new Array(this.n_syn);
+        this.t_spikes = new Array(this.n_syn); // Anzahl Synapsen
         for (let i = 0; i < this.max_spikes; i++) {
-            this.t_spikes[i] = new Array(this.max_spikes);
+            this.t_spikes[i] = new Array(this.max_spikes); // Anzahl der Spikes
 
             for (let ii = 0; ii < this.max_spikes; ii++) {
                 this.t_spikes[i][ii] = -1;
@@ -457,16 +457,58 @@ class LIFSynapticNeuron extends LIFNeuron {
     // Override parent get_input_op method
     get_input_op() {
         // Update our memory of spike times with the new spikes
-        //t_spikes_op = self.update_spike_times()
+        let t_spikes_op = update_spike_times();
+
+        //tf.where(t_spikes_op >=0,
+        //   self.q/self.tau_syn * tf.exp(tf.negative(t_spikes_op/self.tau_syn)),
+        //   t_spikes_op*0.0)
+        // => Gibt ein 2D Shape zurück mit (row, col) der Elemente, die für die Condition True haben
 
         // Evaluate synaptic input current for each spike on each synapse
-        // i_syn_op = tf.where(t_spikes_op >=0,
+        let i_syn_op = new Array(this.n_syn); // Anzahl Synapsen
+        for (let i = 0; i < this.n_syn; i++) {
+            i_syn_op[i] = new Array(this.max_spikes); // Anzahl der Spikes
+
+            for (let ii = 0; ii < this.max_spikes; ii++) {
+                i_syn_op[i][ii] = 0;
+            }
+        }
+
+        //= tf.where(t_spikes_op >=0,
         //                    self.q/self.tau_syn * tf.exp(tf.negative(t_spikes_op/self.tau_syn)),
         //                    t_spikes_op*0.0)
+        //
+        // Ich gehe durch t_spikes_op (die Dimension kenne ich) und prüfe den Eingangsstrom für
+        // jeden gehaltenen Spike i_syn_op hält dann die aktuellen Werte
+        for (let i = 0; i < this.n_syn; i++) {
+            for (let ii = 0; ii < this.max_spikes; ii++) {
+                let x = t_spikes_op[i][ii];
+
+                if (x >= 0) {
+                    // self.q/self.tau_syn * tf.exp(tf.negative(t_spikes_op/self.tau_syn))
+                    // https://de.acervolima.com/tensorflow-js-tf-exp()-funktion/
+                    // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Math/exp
+                    let q_divby_tau = this.q / this.tau_syn; // self.q/self.tau_syn
+                    let neg_t_spikes_op_divby_tau_syn = -1 * (x / this.tau_syn); // tf.exp(tf.negative(t_spikes_op/self.tau_syn))
+
+                    i_syn_op[i][ii] = q_divby_tau * Math.exp(neg_t_spikes_op_divby_tau_syn);
+                } else {
+                    // t_spikes_op*0.0
+                    i_syn_op[i][ii] = 0;
+                }
+            }
+        }
+
 
         // Add each synaptic current to the input current
         //i_op =  tf.reduce_sum(self.w * i_syn_op)
-
-        //return tf.add(self.i_app, i_op)
+        let i_op = 0;
+        for (let i = 0; i < this.n_syn; i++) {
+            for (let ii = 0; ii < this.max_spikes; ii++) {
+                let x = t_spikes_op[i][ii];
+                let i_op = x * this.w;
+                this.i_app = this.i_app + i_op;
+            }
+        }
     }
 }
